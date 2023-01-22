5646fab2747f042db945fbcfa5a99466fcd89460
AdrianoVoltolini <98831167+AdrianoVoltolini@users.noreply.github.com>
2023-01-22 18:53:43 +0100

    pulito

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from ContactNetwork import G
 from SSA import SSA
 from parametri import t, tf
+import time
+
+cnt = 0
+time_start = time.time()
 
 while t < tf:
     t += SSA(G)
-    print(t)
+    cnt += 1
+
+time_end = time.time()
+
+print(f"It took me {time_end-time_start} seconds and {cnt} steps to finish")
--- parametri.py
+++ parametri.py
@@ -5,7 +5,7 @@ num_nodes = 10
 num_edges = 20 # WARNING: non mettere più di N(N-1)/2 edges!
 
 #percentuale della popolazione iniziale
-t0_infetti = 0.2
+t0_infetti = 0.2 
 t0_diagnosed = 0.1
 t0_morti = 0.1
 t0_sani = 1 - t0_infetti - t0_diagnosed - t0_morti
