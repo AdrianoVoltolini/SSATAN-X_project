770ba5206376820f8da1cfee034c342f10578660
AdrianoVoltolini <98831167+AdrianoVoltolini@users.noreply.github.com>
2023-02-01 15:10:44 +0100

    messo in ordine

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+import numpy as np
+import time
+from multiprocessing import Pool
+
 from ContactNetwork import graph_creator
 from SSA import SSA_full
 from SSATANX import SSATANX_full
 from parametri import tf, n_graphs, k, p
-import time
-from multiprocessing import Pool
-import numpy as np
 
 graph_list = [graph_creator(str(x)) for x in range(n_graphs)]
-
 
 #per misurare quanto tempo ci mette
 time_start = time.perf_counter() 
@@ -20,12 +20,12 @@ def graph_elaborator(values):
 
     while t0 < tf:
         # output = SSA_full(G, ass_rates, dis_rates, statuses)
-        output = SSATANX_full(G, tf, t0, ass_rates, dis_rates,k,p, statuses)
+        output = SSATANX_full(G, tf, t0, ass_rates, dis_rates, k, p, statuses)
         t0 += output[0]
         statuses = output[-1]
 
         cnt += 1
-        if cnt % 1000 == 0: # per controllare che stia ancora lavorando
+        if cnt % 10 == 0: # per controllare che stia ancora lavorando
             time_current = time.perf_counter()  
             print(f"After {time_current-time_start:.0f} seconds, graph {G.name} is {(1-((tf-t0)/tf))*100:.0f}% complete")
     
@@ -40,4 +40,5 @@ def graph_elaborator(values):
 # per fare multiprocessing:
 if __name__ == '__main__':
     results = np.array(Pool().map(graph_elaborator,graph_list),dtype=tuple)
-    print(f"Average time: {results[:,0].mean():.2f} seconds, average number of steps: {results[:,1].mean():.2f}")
+    print(f"Time: {results[:,0].mean():.2f} +- {results[:,0].std():.2f} seconds.")
+    print(f"Number of steps: {results[:,1].mean():.0f} +- {results[:,1].std():.0f}")
